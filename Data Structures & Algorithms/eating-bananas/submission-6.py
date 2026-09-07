9d066810e0b3bfd64f25780bcf8d16f3217f9275
Kaamuli <139169554+Kaamuli@users.noreply.github.com>
2026-09-07 16:55:54 +0100

    Add: eating-bananas - submission-6

---
+++ Data Structures & Algorithms/eating-bananas/submission-6.py
@@ -0,0 +1,32 @@
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        piles.sort() #Get it in ascending order
+
+        max_pile = max(piles) #The largest pile is last.
+        k = max_pile
+        
+        #numbers = [i for i in range(1, max_pile + 1)]
+
+        L = 1
+        R = max_pile
+
+        def hourstaken(k):
+            hours_taken = 0
+            for pile in piles:
+                hours_taken += math.ceil(pile/k)
+            return hours_taken 
+
+        while L <= R:
+            M = L + (R-L)//2
+        
+            if hourstaken(M) > h:
+                L = M + 1
+            else:
+                R = M - 1
+                k = min(k,M) 
+        
+        return k
+
+            
